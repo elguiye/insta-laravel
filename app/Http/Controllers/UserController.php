5a253dd2e17e17984255db1116105deea909aebe
Guillermo O <gobregonortiz@gmail.com>
2020-10-23 04:37:03 -0300

    Actualización de datos del usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,25 +11,28 @@ class UserController extends Controller
     }
 
     public function update(Request $request){
+        $user = \Auth::user();
+        $id = $user->id;
 
-        $id = \Auth::user()->id;
-
-       /* $validate = $this->validate($request, [
+       $validate = $this->validate($request, [
             'name' => ['required', 'string', 'max:255'],
             'surname' => ['required', 'string', 'max:255'],
-            'nick' => ['required', 'string', 'max:255', 'unique:users, nick'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-        ]);*/
+            'nick' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+        ]);
      
         $name = $request->input('name');
         $surname = $request->input('surname');
         $nick = $request->input('nick');
         $email = $request->input('email');
 
+        $user->name = $name;
+        $user->surname = $surname;
+        $user->nick = $nick;
+        $user->email = $email;
 
-        var_dump($name);
-        var_dump($surname);
-
-        die();
+        $user->update();
+        return redirect()->route('config')
+                    ->with(['message'=>'Datos actualizados']);
     }
 }
